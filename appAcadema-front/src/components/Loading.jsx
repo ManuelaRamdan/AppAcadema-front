9558956f1530cf403dc6ffe0ed6ccf3b96f4110f
Manuela <mramdan@gmail.com>
2026-05-19 16:07:34 -0300

    cooregi el loading para que este centrado

--- appAcadema-front/src/components/Loading.jsx
+++ appAcadema-front/src/components/Loading.jsx
@@ -1,23 +1,13 @@
 import { TailSpin } from "react-loader-spinner";
 
+
 export default function Loading({
     size = 40,
     color = "#259073",
     fullScreen = false,
 }) {
-    const spinner = (
-        <TailSpin
-            height={size}
-            width={size}
-            color={color}
-            ariaLabel="loading"
-        />
-    );
-
-    const containerClass = fullScreen ? "loading-fullscreen" : "loading-container";
-
     return (
-        <div className={containerClass}>
+        <div className={`flex items-center justify-center w-full`}>
             <TailSpin
                 height={size}
                 width={size}
